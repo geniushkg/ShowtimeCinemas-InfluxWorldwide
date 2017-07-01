8b8b27758ff60a8e3e29e77c12b51d678f33086d
geniushkg <geniushkg@gmail.com>
2017-07-01 14:14:56 +0530

    feat : implement google map

--- app/src/main/java/in/co/influx/showtimecinemas/ShowtimeActivity.java
+++ app/src/main/java/in/co/influx/showtimecinemas/ShowtimeActivity.java
@@ -3,18 +3,32 @@ package in.co.influx.showtimecinemas;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class ShowtimeActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class ShowtimeActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_showtime);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,4 +40,13 @@ public class ShowtimeActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        //13.071778,80.2491799 - influxlatlong
+        LatLng influxplex = new LatLng(13.071778,80.2491799);
+        mMap.addMarker(new MarkerOptions().position(influxplex).title("Influx Worldwide"));
+        mMap.setMinZoomPreference(13);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(influxplex));
+    }
 }
